5c99f1f53581993f78aaa9e85787337b12f87dd5
Andrey Ivanov <prioritet2503@gmail.com>
2022-08-02 14:26:47 +0300

    save

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,7 @@
+import Phonebook from './Phonebook';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-      }}
-    >
-      React homework template
-    </div>
+    <Phonebook />
   );
 };
